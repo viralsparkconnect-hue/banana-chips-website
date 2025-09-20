61c5671946468836d31e2e07c6619fd40ff67699
viralsparkconnect-hue <viralspark.connect@gmail.com>
2025-09-21 01:12:36 +0530

    Create cart.js

---
+++ pages/cart.js
@@ -0,0 +1,5 @@
+// This is the same content as the Shopping Cart Page artifact above
+// Copy the entire content from the "Shopping Cart Page" artifact
+// File location: /pages/cart.js
+
+export { default } from '../artifacts/cart_page'
