3b8a0f45d059c60a5bc138826695cfd67826ea8f
viralsparkconnect-hue <viralspark.connect@gmail.com>
2025-09-21 00:50:15 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,15 @@
-import Link from 'next/link'
-
 export default function Navbar() {
   return (
-    <nav className="bg-yellow-300 p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="font-bold text-xl">🍌 Banana Chips</h1>
-        <div className="space-x-4">
-          <Link href="/" className="hover:underline">Home</Link>
-          <Link href="/about" className="hover:underline">About</Link>
-          <Link href="/products" className="hover:underline">Products</Link>
-          <Link href="/contact" className="hover:underline">Contact</Link>
-        </div>
+    <nav className="bg-yellow-400 p-4 flex justify-between items-center">
+      <div className="text-xl font-bold flex items-center">
+        🍌 Chippyfy
+      </div>
+      <div className="space-x-4">
+        <a href="/" className="hover:underline">Home</a>
+        <a href="/about" className="hover:underline">About</a>
+        <a href="/products" className="hover:underline">Products</a>
+        <a href="/contact" className="hover:underline">Contact</a>
       </div>
     </nav>
-  )
+  );
 }
